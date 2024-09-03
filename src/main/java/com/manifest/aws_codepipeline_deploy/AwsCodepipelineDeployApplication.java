2a8763199e4160a65c8d294a21691c6eabcae784
Peter Togara <petertogara@gmail.com>
2024-09-04 02:22:32 +0300

    iniitial commit

---
+++ src/main/java/com/manifest/aws_codepipeline_deploy/AwsCodepipelineDeployApplication.java
@@ -0,0 +1,13 @@
+package com.manifest.aws_codepipeline_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodepipelineDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodepipelineDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manifest/aws_codepipeline_deploy/AwsCodepipelineDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifest.aws_codepipeline_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodepipelineDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
